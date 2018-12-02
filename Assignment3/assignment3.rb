88214652d6277a19fc38b8a82b1bf154469e446e
PatriciaBlanco <36510755+PatriciaBlanco@users.noreply.github.com>
2018-12-02 15:57:14 +0100

    Final assignment3.rb
    Last update of assignment.rb

--- Assignment3/assignment3.rb
+++ Assignment3/assignment3.rb
@@ -1,24 +1,78 @@
+# Assignment 3    Patricia Blanco Gabella
+# For each gene contained in the provided file it creates a Bio::EMBL object. Then, it and looks for CTTCTT repeats
+# within its exons and saves them as Bio::Feature objects in the corresponding Bio::Sequence object of the gene.
+# Finally it prints this new features in two gff3 files, one with positions referred to the gene, and other referred
+# to the chromosome. It also prints a report with the genes without CTTCTT repeats.
+
 require 'bio'
 require 'net/http'
 
-def add_repeat(bioseq, pos, gene_id, strand)
-	# Add new cttctt repeat feature to a Bio::Sequence object
+#==============Functions==============
+
+def add_repeats (entry, gene_id)
+	# Looks for repeats in exons and adds the feature with the position to the Bio::Sequence object
+	# Returns the Bio::Sequence with the new repeat features or false if no repeat was found
 	
-	f = Bio::Feature.new('cttctt_repeat', pos)
-	f.append(Bio::Feature::Qualifier.new('motif', 'cttctt'))
-	if strand == 1
-		f.append(Bio::Feature::Qualifier.new('strand', '+'))
-	elsif strand == -1
-		f.append(Bio::Feature::Qualifier.new('strand', '-'))
+	pattern_f = Bio::Sequence::NA.new("ctt") # Repeat sequence in forward strand
+	pattern_c = pattern_f.reverse_complement # Repeat sequence in complement strand
+	n_rep = 2 # cttctt = 2 repetitions of the pattern
+	len_rep = (pattern_f*n_rep).length # Total length of the repeat
+	
+	return false unless entry.seq.match(pattern_f*n_rep) or entry.seq.match(pattern_c*n_rep)	# If the sequence doesn't even contain the target sequence return false
+	
+	bioseq = entry.to_biosequence	# Convert Bio::EMBL object to Bio::Sequence object
+	added_pos = [] # Array with the positions of the repeats of the gene that have already been added
+	
+	entry.ft do |feature|	
+		feature_type = feature.feature
+		next unless feature_type == "exon" # If the feature is not an exon pass to next feature
+		locations_obj = feature.locations # Bio::Locations object that contains Bio::Location objects. Usually it has only one Bio::Location object, but it can contain more
+		locations_obj.each do |location|
+			# Each Bio::Location object contains the position of the exon
+			next if location.xref_id # If the position is referred to a remote entry pass 
+			from = location.from - 1		# Start of the exon
+			to = location.to - 1				# End of the exon
+			exon = entry.seq[from..to]	# Exon sequence
+			
+			case location.strand
+			when 1 # Forward strand
+				match = exon.scan(pattern_f) do # Search the motif in the exon 
+					if exon[$~.offset(0)[0],len_rep] == pattern_f*n_rep # If next nucleotides are ctt save position
+						pos = "#{$~.offset(0)[0]+from+1}..#{$~.offset(0)[0]+from+len_rep}"
+						unless added_pos.include? pos
+							# To avoid duplicates. Add new feature unless the repeat position has already been added
+							bioseq.features << create_feature_repeat(pos, gene_id, location.strand)
+							added_pos << pos
+						end
+					end
+				end
+				
+			when -1 # Complement strand
+				match = entry.seq[from..to].scan(pattern_c) do
+					if exon[$~.offset(0)[0],len_rep] == pattern_c*n_rep # If next nucleotides are agg save position
+						pos = "complement(#{$~.offset(0)[0]+from+1}..#{$~.offset(0)[0]+from+len_rep})"
+						unless added_pos.include? pos
+							# To avoid duplicates. Add new feature unless the repeat position has already been added
+							bioseq.features << create_feature_repeat(pos, gene_id, location.strand)
+							added_pos << pos
+						end
+					end				
+				end
+			end
+		end
 	end
-	f.append(Bio::Feature::Qualifier.new('gene', gene_id))
-	bioseq.features << f
+	
+	if added_pos.empty? 
+		return false # If no repeat has been found returns false
+	else
+		return bioseq
+	end
 end
 
 def create_EMBL(gene_id)
 	#Returns Bio::EMBL object using gene_id
 	
-	response = fetch("http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=#{gene_id}")
+	response = fetch("http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=#{gene_id}") # Retrieve entry for that gene id
 	if response == false 
 		puts "Error retrieving gene #{gene_id}. Ignoring..."
 		return false #Unsuccessful retrieval
@@ -45,83 +99,22 @@ def fetch(uri_str)
   end
 end
 
-def find_repeats (entry, gene_id)
+def create_feature_repeat(pos, gene_id, strand)
+	# Add new cttctt repeat feature to a Bio::Sequence object
 	
-	#Look for CTTCTT repeats in exons and return a Bio::Sequence object with the new CTTCTT repeat features
-	
-	return false unless entry.seq.match(/cttctt/) or entry.seq.match(/aagaag/)	# If the sequence doesn't even contain the target sequence pass to next gene
-	
-	bioseq = entry.to_biosequence	# Convert Bio::EMBL object to Bio::Sequence object
-	added_pos = [] # Array with the positions of the CTTCTT repeats of the gene
-		
-	entry.ft do |feature|	
-		feature_type = feature.feature
-		next unless feature_type == "exon" # If the feature is not an exon pass to next feature
-		locations_obj = feature.locations # Bio::Locations object
-		locations_obj.each do |location|
-			# Bio::Location objects with the position
-			next if location.xref_id # If the position is referred to a remote entry pass
-			from = location.from - 1		# Start of the exon
-			to = location.to - 1				# End of the exon
-			exon = entry.seq[from..to]	# Exon sequence
-			
-			case location.strand
-			when 1 # Forward
-				match = exon.scan(/ctt/i) do # Search the motif in the exon 
-					if exon[$~.offset(0)[0],6] == "cttctt" #If next nucleotides are ctt save position
-						pos = "#{$~.offset(0)[0]+from+1}..#{$~.offset(0)[0]+from+6}"
-						unless added_pos.include? pos
-							add_repeat(bioseq, pos, gene_id, location.strand)
-							added_pos << pos
-						end
-					end
-				end
-				
-			when -1 # Complement
-				match = entry.seq[from..to].scan(/aag/i) do
-					if exon[$~.offset(0)[0],6] == "aagaag"
-						pos = "complement(#{$~.offset(0)[0]+from+1}..#{$~.offset(0)[0]+from+6})"
-						unless added_pos.include? pos
-							add_repeat(bioseq, pos, gene_id, location.strand)
-							added_pos << pos
-						end
-					end				
-				end
-			end
-		end
+	f = Bio::Feature.new('cttctt_repeat', pos)
+	f.append(Bio::Feature::Qualifier.new('motif', 'cttctt'))
+	if strand == 1
+		f.append(Bio::Feature::Qualifier.new('strand', '+'))
+	elsif strand == -1
+		f.append(Bio::Feature::Qualifier.new('strand', '-'))
 	end
-	
-	if added_pos.empty? 
-		return false # If no CTTCTT repeat has been found returns false
-	else
-		return bioseq
-	end
-end
-
-def write_gff3_gene (targets)
-	#Write a gff3 file
-	
-	gff3 = File.open("results.gff3", 'w')
-	gff3.puts "##gff-version 3\n"
-	
-	targets.each do |gene_id,bioseq|
-		n_id = 1
-		bioseq.features.each do |feature|
-			featuretype = feature.feature
-			next unless featuretype == "cttctt_repeat"
-			loc = feature.locations.first
-			strand = feature.assoc["strand"]
-			my_id = "CTTCTT_repeat_#{gene_id}_#{n_id}"
-			
-			#seqid	source	start	end	score	strand	phase	attributes
-			gff3.puts "#{gene_id}\tAssignment3\trepeat_region\t#{loc.from}\t#{loc.to}\t.\t#{strand}\t.\tID=#{my_id};Name=repeat_cttctt"
-			n_id += 1
-		end			
-	end
+	f.append(Bio::Feature::Qualifier.new('gene', gene_id))
+	return f
 end
 
 def write_gff3_chr (targets)
-	#Write a gff3 file with chromosome coordinates
+	# Write a gff3 file with chromosome coordinates
 	
 	chr_gff3 = File.open("chr_results.gff3", 'w')
 	chr_gff3.puts "##gff-version 3\n"
@@ -143,13 +136,37 @@ def write_gff3_chr (targets)
 	end
 end
 
+def write_gff3_gene (targets)
+	# Write a gff3 file
+	
+	gff3 = File.open("results.gff3", 'w')
+	gff3.puts "##gff-version 3\n"
+	
+	targets.each do |gene_id,bioseq|
+		n_id = 1
+		bioseq.features.each do |feature|
+			featuretype = feature.feature
+			next unless featuretype == "cttctt_repeat"
+			loc = feature.locations.first
+			strand = feature.assoc["strand"]
+			my_id = "CTTCTT_repeat_#{gene_id}_#{n_id}"
+			
+			#seqid	source	start	end	score	strand	phase	attributes
+			gff3.puts "#{gene_id}\tAssignment3\trepeat_region\t#{loc.from}\t#{loc.to}\t.\t#{strand}\t.\tID=#{my_id};Name=repeat_cttctt"
+			n_id += 1
+		end			
+	end
+end
+
+#===============================================================================================================
+
 usage = "usage: ruby assignment3.rb ArabidopsisSubNetwork_GeneList.txt"
 abort (usage) if ARGV.length != 1 # Check if the gene list file is present
 abort ("Couldn't find the file #{ARGV[0]}") unless File.file?(ARGV[0]) # Check if the file exists
 
-puts "\nSearching CTTCTT repeats...\n\n"
+print "\nSearching repeats..."
 genes = {}			# Hash with all Bio::EMBL objects for the genes in the list (gene_id => Bio::EMBL)
-targets = {}		# Hash with the Bio::Sequence::NA objects with cttctt in exons (gene_id => Bio::Sequence::NA)
+targets = {}		# Hash with the Bio::Sequence objects with the target in exons (gene_id => Bio::Sequence)
 
 File.readlines(ARGV[0]).each do |gene_id|
 	gene_id = gene_id.chomp
@@ -160,14 +177,17 @@ File.readlines(ARGV[0]).each do |gene_id|
 		next # Unsuccessful retrieval
 	end
 	
-	bioseq = find_repeats(entry,gene_id) # Bio::Sequence object with the new cttctt repeat features or false if no repeat was found
-	targets [gene_id] = bioseq unless bioseq == false	
+	bioseq = add_repeats(entry,gene_id) # Bio::Sequence object with the new repeat features or false if no repeat was found
+	targets [gene_id] = bioseq unless bioseq == false
+	
 end
+puts "Done\n\n"
 
+#Write gff3 file with the repeats
 write_gff3_gene(targets)
 write_gff3_chr(targets)
 
-#Write report with genes without cttctt repeats
+#Write report with genes without repeats
 no_rep = File.open("no_repeat.txt", 'w')
 genes.keys.each do |gene|
 	unless targets.keys.include? gene
@@ -175,5 +195,5 @@ genes.keys.each do |gene|
 	end
 end
 
-puts "CTTCTT repeats have been saved in repeats.gff3 (gene coordinates) and chr_repeats.gff3 (chromosome coordinates)"
-puts "Genes without CTTCTT repeats in exons have been saved in no_repeats.txt"
+puts "Found repeats have been saved in repeats.gff3 (gene coordinates) and chr_repeats.gff3 (chromosome coordinates)"
+puts "Genes without repeats in exons have been saved in no_repeats.txt"
